294bc2e6a2da95c388ef615314fff18e5cb00141
Andreas Schürmann <andreas@adhook.io>
2020-02-06 14:27:51 +0100

    Initial Commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 function main () {
   let server = express();
 
-  // TODO: Basic Auth
+  // TODO: JWT Auth
 
   const port = process.env.PORT || 8000;
   server.listen(port, () => console.log(`Server is listening on port: ${port}`));
