a1837dd989a657587b5c60ac9941b0ef308a69ed
Andreas Schürmann <andreas@adhook.io>
2021-02-12 11:38:15 +0100

    Added cors demo

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ const express = require('express');
 const server = express();
 
 server.get('/heroes', (req, res) => {
-  res.send([{ name: 'Superman' }]);
+  res.send([
+    { id: 11, name: 'Dr Nice from Server' },
+    { id: 12, name: 'Narco from Server' },
+    { id: 13, name: 'Bombasto from Server' },
+    { id: 14, name: 'Celeritas from Server' },
+    { id: 15, name: 'Magneta from Server' },
+    { id: 16, name: 'RubberMan from Server' },
+    { id: 17, name: 'Dynama from Server' },
+    { id: 18, name: 'Dr IQ from Server' },
+    { id: 19, name: 'Magma from Server' },
+    { id: 20, name: 'Tornado from Server' }
+  ]);
 });
 
 server.listen(4566, () => {
